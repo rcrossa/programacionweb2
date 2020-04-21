577fc1efadf5239280b9912bf628b8136c19d117
Roberto Rossa <rcrossa@hotmail.com>
2020-04-21 19:46:24 -0300

    creacion de producto
    se agrega imagenes

--- Roberto-Rossa/productos.php
+++ Roberto-Rossa/productos.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style/style-robert.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="recuadro-imagenes">
+       <div class="imagenes-paquete">
+           <div class=img-1> 
+           <h1>Test</h1>
+           <img src="material-rob/img/publicacion1/Untitled-1.png" alt="imagen de acantilado">
+           </div>
+           <div class=img-1> 
+           <h1>Test</h1>
+           <img src="material-rob/img/publicacion1/Untitled-2.png" alt="imagen de acantilado">
+           </div>
+           <div class=img-1> 
+           <h1>Test</h1>
+           <img src="material-rob/img/publicacion1/Untitled-4.png" alt="imagen de acantilado">
+           </div>
+       </div>
+    </div>
 </body>
 </html>

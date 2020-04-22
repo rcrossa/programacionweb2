2e0edd89a13f87a4aa4dbee4c289c80cc422a0da
Roberto Rossa <rcrossa@hotmail.com>
2020-04-22 01:11:52 -0300

    update
    Se actualizaron las imagenes con modal y descripcion de prueba

--- Roberto-Rossa/productos.php
+++ Roberto-Rossa/productos.php
@@ -12,168 +12,184 @@
 </head>
 
 <body>
-<script>
-$(document).ready(function(){
-  $('[data-toggle="popover"]').popover();   
-});
-</script>
-    <div class="container">
+
+    <!-- <div class="container">
         <div class="row">
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 shadow-lg p-2 mb-2 bg-white">
-                <img src="material-rob/img/publicacion1/Untitled-3.png" data-toggle="popover" data-placement="top" width="250" height="250"
+                <img src="material-rob/img/publicacion1/Untitled-3.png" width="350" height="250"
                     alt="imagen de acantilado">
             </div>
 
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 shadow-lg p-2 mb-2 bg-white">
-                <img src="material-rob/img/publicacion1/Untitled-2.png" width="250" height="250"
+                <img src="material-rob/img/publicacion1/Untitled-2.png" width="350" height="250"
                     alt="imagen de acantilado">
             </div>
 
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 shadow-lg p-2 mb-2 bg-white">
-                <img src="material-rob/img/publicacion1/Untitled-4.png" width="250" height="250"
+                <img src="material-rob/img/publicacion1/Untitled-4.png" width="350" height="250"
                     alt="imagen de acantilado">
             </div>
 
         </div>
-    </div>
+    </div> -->
 
- <div class="imagen1">
- <div class="container">
-      <div class="row">
-              <!-- <h2>Fading Modal</h2>
-        <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
+    <div class="imagen1">
+        <div class="container">
+            <div class="row">
+               <!-- <h2>Fading Modal</h2>
+                  <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> --> 
 
-        <!-- Button to Open the Modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-            <img src="material-rob/img/publicacion1/Untitled-3.png" width="250" height="250" alt="imagen de acantilado">
-        </button>
-       
-        <!-- The Modal -->
+                <!-- Button to Open the Modal -->
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" data-toggle="modal" data-target="#myModal">
+                    <img src="material-rob/img/publicacion1/Untitled-3.png" width="350" height="250"
+                        alt="imagen de acantilado">
+                </div>
 
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <div class="modal fade" id="myModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
+                <!-- The Modal -->
 
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                        <h4 class="modal-title">Imagen 1</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="modal fade" id="myModal">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Imagen 1</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <!-- Modal body -->
+                                <div class="modal-body">
+                                    <img src="material-rob/img/publicacion1/Untitled-3.png" width="480" height="222"
+                                        alt="imagen de acantilado">
+                                    <p>Incluye:
+                                        - Pasaje aéreo de ida y vuelta en clase económica con restricciones de Air
+                                        France (salida mayo) y Swiss-Lufthansa(salida: septiembre).
+                                    </p>
+                                    <p>- Alojamiento en hoteles categoría 4 estrellas (en Ljubljana, Zagreb y Varsovia
+                                        hoteles 5*) ubicados en planta urbana en habitaciones dobles standard con baño
+                                        privado, incluyendo desayuno buffet y comidas donde se indica, con menú fijo e
+                                        idéntico para todos los pasajeros que podrá ser con sistema buffet o servido en
+                                        la mesa.
+                                        - Transporte en autocar privado.
+                                        - Excursiones y visitas con guía de habla hispana; entradas a museos y
+                                        monumentos indicadas en el programa.
+                                        - Propinas obligatorias a maleteros en los hoteles (una maleta por persona).
+                                        - Acompañamiento de un representante de la empresa en el tramo aéreo Buenos
+                                        Aires/Belgrado y durante todo el recorrido terrestre (no incluye acompañamiento
+                                        en los vuelos de regreso).
+                                    </p>
+                                </div>
+
+
+                                <!-- Modal footer -->
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-
-                    <!-- Modal body -->
-                    <div class="modal-body">
-                        <img src="material-rob/img/publicacion1/Untitled-3.png" width="480" height="222"
-                            alt="imagen de acantilado">
-                    </div>
-                    
-
-                    <!-- Modal footer -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    </div>
-
                 </div>
             </div>
+
         </div>
-        </div>
-      </div>
+    </div> 
+ 
+    <div class="imagen2">
+        <div class="container">
+            <div class="row">
+                <!-- <h2>Fading Modal</h2>
+                <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
 
-    </div>
- </div>
+                <!-- Button to Open the Modal -->
 
- <div class="imagen2">
- <div class="container">
-      <div class="row">
-              <!-- <h2>Fading Modal</h2>
-        <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" data-toggle="modal" data-target="#myModal1">
+                    <img src="material-rob/img/publicacion1/Untitled-2.png" width="350" height="250"
+                        alt="imagen de acantilado">
+                </div>
 
-        <!-- Button to Open the Modal -->
-        
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal1">
-            <img src="material-rob/img/publicacion1/Untitled-2.png" width="250" height="250" alt="imagen de acantilado">
-        </button>
-        
-        <!-- The Modal -->
+                <!-- The Modal -->
 
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <div class="modal fade" id="myModal1">
-            <div class="modal-dialog">
-                <div class="modal-content">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="modal fade" id="myModal1">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
 
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                        <h4 class="modal-title">Imagen 1</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Imagen 1</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <!-- Modal body -->
+                                <div class="modal-body">
+                                    <img src="material-rob/img/publicacion1/Untitled-2.png" width="480" height="222"
+                                        alt="imagen de acantilado">
+                                </div>
+
+
+                                <!-- Modal footer -->
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-
-                    <!-- Modal body -->
-                    <div class="modal-body">
-                        <img src="material-rob/img/publicacion1/Untitled-2.png" width="480" height="222"
-                            alt="imagen de acantilado">
-                    </div>
-                    
-
-                    <!-- Modal footer -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    </div>
-
                 </div>
             </div>
-        </div>
-        </div>
-      </div>
 
+        </div>
     </div>
- </div>   
 
     <div class="imagen3">
-    <div class="container">
-      <div class="row">
-              <!-- <h2>Fading Modal</h2>
-        <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
+        <div class="container">
+            <div class="row">
+                <!-- <h2>Fading Modal</h2>
+                    <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
 
-        <!-- Button to Open the Modal -->
-        <button type="button" class="btn btn-secondary btn-lg" data-toggle="modal" data-target="#myModal2">
-            <img src="material-rob/img/publicacion1/Untitled-4.png" width="250" height="250" alt="imagen de acantilado">
-        </button>
-        <!-- The Modal -->
+                <!-- Button to Open the Modal -->
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" data-toggle="modal" data-target="#myModal2">
+                    <img src="material-rob/img/publicacion1/Untitled-4.png" width="350" height="250"
+                        alt="imagen de acantilado">
+                </div>
+                <!-- The Modal -->
 
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <div class="modal fade" id="myModal2">
-            <div class="modal-dialog">
-                <div class="modal-content">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="modal fade" id="myModal2">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Imagen 1</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                                <!-- Modal body -->
+                                <div class="modal-body">
+                                    <img src="material-rob/img/publicacion1/Untitled-4.png" width="480" height="222"
+                                        alt="imagen de acantilado">
+                                </div>
+                                <!-- Modal footer -->
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
 
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                        <h4 class="modal-title">Imagen 1</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+                        </div>
                     </div>
-
-                    <!-- Modal body -->
-                    <div class="modal-body">
-                        <img src="material-rob/img/publicacion1/Untitled-4.png" width="480" height="222"
-                            alt="imagen de acantilado">
-                    </div>
-                    
-
-                    <!-- Modal footer -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    </div>
-
                 </div>
             </div>
-        </div>
-        </div>
-      </div>
 
-    </div>
-    </div>
+        </div>
+    </div> 
+ 
+ 
+
     <script src="../js/jquery/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
     <script src="../js/popper/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
     <script src="../js/bootstrap/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+
 
 </body>
 

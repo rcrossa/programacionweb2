c2b3abaf3ec5248e7789cffc2e05ccd10b23f0d9
Roberto Rossa <rcrossa@hotmail.com>
2020-04-21 22:24:52 -0300

    update
    actualizando productos

--- Roberto-Rossa/productos.php
+++ Roberto-Rossa/productos.php
@@ -1,27 +1,180 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style/style-robert.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,600,700,700i&amp;subset=latin-ext"
+        rel="stylesheet">
+    <link href="../css/bootstrap/bootstrap.css" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
-    <div class="recuadro-imagenes">
-       <div class="imagenes-paquete">
-           <div class=img-1> 
-           <h1>Test</h1>
-           <img src="material-rob/img/publicacion1/Untitled-1.png" alt="imagen de acantilado">
-           </div>
-           <div class=img-1> 
-           <h1>Test</h1>
-           <img src="material-rob/img/publicacion1/Untitled-2.png" alt="imagen de acantilado">
-           </div>
-           <div class=img-1> 
-           <h1>Test</h1>
-           <img src="material-rob/img/publicacion1/Untitled-4.png" alt="imagen de acantilado">
-           </div>
-       </div>
+<script>
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();   
+});
+</script>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 shadow-lg p-2 mb-2 bg-white">
+                <img src="material-rob/img/publicacion1/Untitled-3.png" data-toggle="popover" data-placement="top" width="250" height="250"
+                    alt="imagen de acantilado">
+            </div>
+
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 shadow-lg p-2 mb-2 bg-white">
+                <img src="material-rob/img/publicacion1/Untitled-2.png" width="250" height="250"
+                    alt="imagen de acantilado">
+            </div>
+
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 shadow-lg p-2 mb-2 bg-white">
+                <img src="material-rob/img/publicacion1/Untitled-4.png" width="250" height="250"
+                    alt="imagen de acantilado">
+            </div>
+
+        </div>
     </div>
+
+ <div class="imagen1">
+ <div class="container">
+      <div class="row">
+              <!-- <h2>Fading Modal</h2>
+        <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
+
+        <!-- Button to Open the Modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+            <img src="material-rob/img/publicacion1/Untitled-3.png" width="250" height="250" alt="imagen de acantilado">
+        </button>
+       
+        <!-- The Modal -->
+
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="modal fade" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Imagen 1</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <img src="material-rob/img/publicacion1/Untitled-3.png" width="480" height="222"
+                            alt="imagen de acantilado">
+                    </div>
+                    
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+      </div>
+
+    </div>
+ </div>
+
+ <div class="imagen2">
+ <div class="container">
+      <div class="row">
+              <!-- <h2>Fading Modal</h2>
+        <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
+
+        <!-- Button to Open the Modal -->
+        
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal1">
+            <img src="material-rob/img/publicacion1/Untitled-2.png" width="250" height="250" alt="imagen de acantilado">
+        </button>
+        
+        <!-- The Modal -->
+
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="modal fade" id="myModal1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Imagen 1</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <img src="material-rob/img/publicacion1/Untitled-2.png" width="480" height="222"
+                            alt="imagen de acantilado">
+                    </div>
+                    
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+      </div>
+
+    </div>
+ </div>   
+
+    <div class="imagen3">
+    <div class="container">
+      <div class="row">
+              <!-- <h2>Fading Modal</h2>
+        <p>Add the "fade" class to the modal container if you want the modal to fade in on open and fade out on close.</p> -->
+
+        <!-- Button to Open the Modal -->
+        <button type="button" class="btn btn-secondary btn-lg" data-toggle="modal" data-target="#myModal2">
+            <img src="material-rob/img/publicacion1/Untitled-4.png" width="250" height="250" alt="imagen de acantilado">
+        </button>
+        <!-- The Modal -->
+
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div class="modal fade" id="myModal2">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Imagen 1</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <img src="material-rob/img/publicacion1/Untitled-4.png" width="480" height="222"
+                            alt="imagen de acantilado">
+                    </div>
+                    
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+      </div>
+
+    </div>
+    </div>
+    <script src="../js/jquery/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="../js/popper/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="../js/bootstrap/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+
 </body>
+
 </html>

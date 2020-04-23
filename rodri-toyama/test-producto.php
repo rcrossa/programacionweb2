4148e4d51e80a5b07ce676d2b7a925239b751aa2
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-22 22:02:36 -0300

    agregado mi carpeta, index.php con diseno de filtro y cards de productos

---
+++ rodri-toyama/test-producto.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <p class="card-text">Nombre: <span>name</span></p>
+      <p class="card-text">Precio: <span>price</span></p>
+      <p class="card-text">Descripcion: <span>desc</span></p>
+    </div>
+  </div>
+
+</body>
+
+</html>
+
+
+<?php
+
+$producto = array(
+    'id_producto' => 10,
+    'nombre' => 'Cerro Castor, Usuahia, Argentina',
+    'precio' => '$50000',
+    'descripción' => 'Un lugar perfecto para esquiar, disfrutá tu país, disfrutá Usuahia',
+);
+
+/*
+{
+  "productos": [
+    {
+      "id": 10
+      'nombre': 'Cerro Castor, Usuahia, Argentina',
+      'precio': '$50000',
+      'descripción': 'Un lugar perfecto para esquiar, disfrutá tu país, disfrutá Usuahia',
+    },
+    {
+      "id": 11
+      'nombre': 'Cerro Castor, Usuahia, Argentina',
+      'precio': '$50000',
+      'descripción': 'Un lugar perfecto para esquiar, disfrutá tu país, disfrutá Usuahia',
+    }
+  ]
+};
+*/
+
+
+
+?>

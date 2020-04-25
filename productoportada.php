e72074d0e380eed5788f2412ad3252d774a096b2
Roberto Rossa <rcrossa@hotmail.com>
2020-04-25 15:43:49 -0300

    actulizacion
    Se realiza actualizacion de productos portada. Se actualiza el archivo php con los nuevos array. Se coloca el cierre de la etiqueta php en productos.php en la carpeta de rodrigo.

--- productoportada.php
+++ productoportada.php
@@ -1,73 +1,84 @@
-<div class="container">
-    <div class="row pb-5">
+<div class="container-fluid px-5">
+            <div class="row justify-content-center">
 
-        <div class="col-sm-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="filter-wrap py-4">
-                <h3>Filtro</h3>
-                <div class="filter-border p-4 border border-secondary">
-                    <div class="filter-inner">
-                        <div class="filtrolugar">
-                            <div class="row justify-content-center align-items-center">
-                                <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 py-2">
-                                    <select class="custom-select custom-select-lg">
-                                        <option selected></option>
-                                        <option value="0">Lugar</option>
-                                        <option value="1">Interior</option>
-                                        <option value="2">Exterior</option>
-                                    </select>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 py-2">
-                                    <select class="custom-select custom-select-lg">
-                                        <option selected></option>
-                                        <option value="0">Tipo de viaje</option>
-                                        <option value="1">Premium</option>
-                                        <option value="2">Economico</option>
-                                    </select>
-                                </div>
-                                <div class="row button">
-                                    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-                                        <button class="btn btn-danger btn-md px-4">Buscar</button>
+                <div class="col-sm-12 col-md-12 col-lg-3">
+                    <div class="filter-wrap py-4">
+                        <h3>Filtro</h3>
+                        <div class="filter-border p-4 border border-secondary">
+                            <div class="filter-inner">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <select class="custom-select custom-select-lg mb-3">
+                                            <option selected>Interior</option>
+                                            <?php foreach ($interior as $key => $value) : ?>
+                                                <option value="<?php echo $key ?>"><?php echo $interior[$key]["nombre"]; ?></option>
+                                            <?php endforeach ?>
+                                        </select>
+                                    </div>
+                                    <div class="col-12">
+                                        <select class="custom-select custom-select-lg mb-3">
+                                            <option selected>Exterior</option>
+                                            <?php foreach ($exterior as $key => $value) : ?>
+                                                <option value="<?php echo $key ?>"><?php echo $exterior[$key]["nombre"]; ?></option>
+                                            <?php endforeach ?>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
+                            <div class="button">
+                                <button class="btn btn-danger btn-lg px-5 btn-block">Buscar</button>
+                            </div>
                         </div>
-
                     </div>
-
                 </div>
-            </div>
-        </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-8">
+                    <div class="row">
 
-                <?php foreach ($productos as $key => $value) : ?>
+                        <?php foreach ($interior as $key => $value) : ?>
 
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 my-3">
-                        <div class="card carta">
-                            <img src="https://images.pexels.com/photos/1450353/pexels-photo-1450353.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title1"><?php echo $productos[$key]["nombre"]; ?></h5>
-                                <p class="card-text"><?php echo $productos[$key]["descripcion"]; ?></p>
-                                <div class="row justify-content-center pt-1 pb-3">
-                                    <h5>
-                                        <span class="card-text text-center badge badge-light"><?php echo $productos[$key]["precio"]; ?></span>
-                                    </h5>
-                                </div>
-                                <div class="container">
-                                    <div class="row justify-content-around pb-2">
-                                        <a href="#" class="btn btn-success">Comprar</a>
-                                        <a href="#" class="btn btn-outline-primary">Ver mas</a>
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-4 my-3">
+                                <div class="card">
+
+                                    <?php echo '<img src="' .  $interior[$key]["url"] . '" class="card-img-top" alt="..." />' ?>
+
+                                    <div class="card-body">
+                                        <h5 class="card-title1"><?php echo $interior[$key]["nombre"]; ?></h5>
+                                        <p class="card-text"><?php echo $interior[$key]["descripcion"]; ?></p>
+                                        <p class="card-text text-center"><?php echo $interior[$key]["precio"]; ?></p>
+                                        <div class="container d-flex justify-content-around">
+                                            <a href="#" class="btn btn-success">Comprar</a>
+                                            <a href="#" class="btn btn-outline-success">Reservar</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
 
-                <?php endforeach ?>
+                        <?php endforeach ?>
+
+                        <?php foreach ($exterior as $key => $value) : ?>
+
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-4 my-3">
+                                <div class="card">
+
+                                    <?php echo '<img src="' .  $exterior[$key]["url"] . '" class="card-img-top" alt="..." />' ?>
+
+                                    <div class="card-body">
+                                        <h5 class="card-title1"><?php echo $exterior[$key]["nombre"]; ?></h5>
+                                        <p class="card-text"><?php echo $exterior[$key]["descripcion"]; ?></p>
+                                        <p class="card-text text-center"><?php echo $exterior[$key]["precio"]; ?></p>
+                                        <div class="container d-flex justify-content-around">
+                                            <a href="#" class="btn btn-success">Comprar</a>
+                                            <a href="#" class="btn btn-outline-success">Reservar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php endforeach ?>
+
+                    </div>
+                </div>
 
             </div>
         </div>
-
-    </div>
-</div>

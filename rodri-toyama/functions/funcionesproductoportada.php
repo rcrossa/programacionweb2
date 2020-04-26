7dcf62624ec2b0f2b39079cfe8397c8d4aa6ea7a
Roberto Rossa <rcrossa@hotmail.com>
2020-04-26 17:10:23 -0300

    Update funcionesproductoportada.php
    Se actualizan descripciones y se elimina lorem ipsum.

--- rodri-toyama/functions/funcionesproductoportada.php
+++ rodri-toyama/functions/funcionesproductoportada.php
@@ -5,42 +5,42 @@ $interior = array(
         "id" => 0,
         'nombre' => 'Ushuaia',
         'precio' => '$50000',
-        'descripcion' => 'descripcion Ushuaia Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Consultar Incluye: Aéreos Buenos Aires/Ushuaia/Buenos Aires Traslados de llegada y salida Alojamiento según régimen seleccionado Coordinador Menores: Los menores de 2 años viajan sin cargo Equipaje: Incluye un equipaje en bodega',
         'url' => './images/ushuaia.jpeg'
     ),
     array(
         'id' => 1,
-        'nombre' => 'Chubut',
+        'nombre' => 'Calafate',
         'precio' => '$45000',
-        'descripcion' => 'descripcion Chubut Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas - Consultar Incluye: Aéreos Buenos Aires/Calafate/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y salida Alojamiento según régimen seleccionado',
         'url' => './images/chubut.jpg'
     ),
     array(
         'id' => 2,
-        'nombre' => 'Catamarca',
+        'nombre' => 'La Rioja',
         'precio' => '$60000',
-        'descripcion' => 'descripcion Rio Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborumasd asdasd',
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Jueves Incluye: Pasaje aéreo Buenos Aires/San Juan/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y de salida Alojamiento según régimen seleccionado ( 2 nts en San Juan, 1 nt en Villa Union ). Excursiones incluidas.',
         'url' => './images/rio.jpeg'
     ),
     array(
         'id' => 3,
-        'nombre' => 'Cordoba',
+        'nombre' => 'Bariloche',
         'precio' => '$45000',
-        'descripcion' => 'descripcion Cordoba Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Duración: 6 Días / 5 Noches Salidas: - Lunes Incluye: Aéreos Buenos Aires/Bariloche/Buenos Aires Traslados de llegada y de salida Alojamiento: Según régimen seleccionado. Coordinador. Menores: Los menores de 2 años viajan sin cargo y sin servicio.',
         'url' => './images/cordoba.jpeg'
     ),
     array(
         'id' => 4,
         'nombre' => 'Misiones',
         'precio' => '$20000',
-        'descripcion' => 'descripcion Misiones Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Duración: 4 días / 3 noches Salidas: - A consultar Incluye: Pasaje aéreo Buenos Aires/Puerto Iguazu/Buenos Aires Incluye un equipaje de mano de hasta 8kg Traslados de llegada y salida Alojamiento según régimen seleccionado Coordinador. No incluye: 30% del impuesto pais.',
         'url' => './images/misiones.jpeg'
     ),
     array(
         'id' => 5,
         'nombre' => 'Mendoza',
         'precio' => '$36000',
-        'descripcion' => 'descripcion Mendoza Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Jueves Incluye: Aéreos Buenos Aires/Mendoza/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y de salida Alojamiento según régimen seleccionado.',
         'url' => './images/mendoza.jpeg'
     ),
 );
@@ -48,44 +48,44 @@ $interior = array(
 $exterior = array(
     array(
         "id" => 0,
-        'nombre' => 'Brasil',
+        'nombre' => 'Rio',
         'precio' => '$50000',
-        'descripcion' => 'descripcion Brasil Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Programa incluye: Vuelos operados por Gol en clase "O" con equipaje incluido y venta anticipada de 28 días. Traslados IN/OUT en regular. 07 noches de alojamiento según régimen indicado. Opcional: asistencia CORIS con seguro de cancelación USD 36 por persona.',
         'url' => './images/ushuaia.jpeg'
     ),
     array(
         'id' => 1,
-        'nombre' => 'Mexico',
+        'nombre' => 'Cancun',
         'precio' => '$45000',
-        'descripcion' => 'descripcion Mexico Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Incluye: Ticket aéreo, Traslados, Alojamiento según régimen y Asistencia al viajero.',
         'url' => './images/chubut.jpg'
     ),
     array(
         'id' => 2,
         'nombre' => 'Peru',
         'precio' => '$60000',
-        'descripcion' => 'descripcion Peru Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborumasd asdasd',
+        'descripcion' => 'EL PROGRAMA INCLUYE:   GUÍA: Operado con guía en español e inglés.   ALOJAMIENTO: 3 noches de alojamiento en Lima. 1 noche de alojamiento en Paracas. 2 noches de alojamiento en Cusco. 1 noche de alojamiento en Aguas Calientes. ALIMENTACIÓN: Desayuno diario incluidos.',
         'url' => './images/rio.jpeg'
     ),
     array(
         'id' => 3,
         'nombre' => 'España',
         'precio' => '$45000',
-        'descripcion' => 'descripcion España Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'ISITANDO: MADRID - MEAÑO - RIAS BAJAS - SANTIAGO DE COMPOSTELA - PLAYA DE LAS CATEDRALES - RINLO - CABO VIDIO - LA MANJOYA - COVADONGA - CUEVAS DEL SOPLAO - SANTANDER - BILBAO - SAN SEBASTIAN - PAMPLONA - HARO - QUINTANILLA DE ONESIMO.',
         'url' => './images/cordoba.jpeg'
     ),
     array(
         'id' => 4,
         'nombre' => 'Estados Unidos',
         'precio' => '$20000',
-        'descripcion' => 'descripcion Estados Unidos Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'Visitando: Boston / Quebec / Montreal / Ottawa / Toronto / Niagara Falls - Incluye: Desayunos Americanos.',
         'url' => './images/misiones.jpeg'
     ),
     array(
         'id' => 5,
         'nombre' => 'Canada',
         'precio' => '$36000',
-        'descripcion' => 'descripcion Canada Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores velit laborum',
+        'descripcion' => 'VISITANDO: Vancouver/ Hope / Kelowna / Revelstoke / Golden / Columbia Ice Field / Canmore /  Banff / Calgar.',
         'url' => './images/mendoza.jpeg'
     ),
 );

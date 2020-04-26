ea68f7e6ec1f41a36c24f1260afae4c09500ee4b
Roberto Rossa <rcrossa@hotmail.com>
2020-04-26 18:17:30 -0300

    actualizacion productos
    Se actualizan las imagenes y las descripciones

--- rodri-toyama/functions/funcionesproductoportada.php
+++ rodri-toyama/functions/funcionesproductoportada.php
@@ -5,43 +5,43 @@ $interior = array(
         "id" => 0,
         'nombre' => 'Ushuaia',
         'precio' => '$50000',
-        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Consultar Incluye: Aéreos Buenos Aires/Ushuaia/Buenos Aires Traslados de llegada y salida Alojamiento según régimen seleccionado Coordinador Menores: Los menores de 2 años viajan sin cargo Equipaje: Incluye un equipaje en bodega',
-        'url' => 'https://www.mktespider.com.ar/usr/images/amichi/DESTINOS/USH/6.jpg'
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Consultar Incluye: Aéreos Buenos Aires/Ushuaia/Buenos Aires Traslados de llegada y salida Alojamiento según régimen seleccionado Coordinador. Menores de 2 años viajan sin cargo Equipaje: Incluye un equipaje en bodega',
+        'url' => '../images/ushuaia.jpg'
     ),
     array(
         'id' => 1,
         'nombre' => 'Calafate',
         'precio' => '$45000',
-        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas - Consultar Incluye: Aéreos Buenos Aires/Calafate/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y salida Alojamiento según régimen seleccionado',
-        'url' => 'https://www.mktespider.com.ar/usr/images/amichi/DESTINOS/FTE/8.jpg'
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas - Consultar Incluye: Aéreos Buenos Aires/Calafate/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y salida Alojamiento según régimen seleccionado. Menores de 2 años sin cargo. Incluye equipaje',
+        'url' => '../images/calafate.jpg'
     ),
     array(
         'id' => 2,
         'nombre' => 'La Rioja',
         'precio' => '$60000',
-        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Jueves Incluye: Pasaje aéreo Buenos Aires/San Juan/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y de salida Alojamiento según régimen seleccionado ( 2 nts en San Juan, 1 nt en Villa Union ). Excursiones incluidas.',
-        'url' => 'https://www.mktespider.com.ar/usr/images/amichi/DESTINOS/UAQ/8.jpg'
+        'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Jueves Incluye: Pasaje aéreo Buenos Aires/San Juan/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y de salida Alojamiento según régimen seleccionado ( 2 nts en San Juan, 1 nt en Villa Union ).',
+        'url' => '../images/larioja.jpg'
     ),
     array(
         'id' => 3,
         'nombre' => 'Bariloche',
         'precio' => '$45000',
         'descripcion' => 'Duración: 6 Días / 5 Noches Salidas: - Lunes Incluye: Aéreos Buenos Aires/Bariloche/Buenos Aires Traslados de llegada y de salida Alojamiento: Según régimen seleccionado. Coordinador. Menores: Los menores de 2 años viajan sin cargo y sin servicio.',
-        'url' => 'https://www.mktespider.com.ar/usr/images/amichi/DESTINOS/BRC/8.jpg'
+        'url' => '../images/bariloche.jpg'
     ),
     array(
         'id' => 4,
         'nombre' => 'Misiones',
         'precio' => '$20000',
-        'descripcion' => 'Duración: 4 días / 3 noches Salidas: - A consultar Incluye: Pasaje aéreo Buenos Aires/Puerto Iguazu/Buenos Aires Incluye un equipaje de mano de hasta 8kg Traslados de llegada y salida Alojamiento según régimen seleccionado Coordinador. No incluye: 30% del impuesto pais.',
-        'url' => 'https://www.mktespider.com.ar/usr/images/amichi/DESTINOS/IGR/3.jpg'
+        'descripcion' => 'Duración: 4 días / 3 noches Salidas: - A consultar Incluye: Pasaje aéreo Buenos Aires/Puerto Iguazu/Buenos Aires Incluye un equipaje de mano de hasta 8kg Traslados de llegada y salida Alojamiento según régimen seleccionado Coordinador. No incluye: impuesto pais.',
+        'url' => '../images/misiones.jpeg'
     ),
     array(
         'id' => 5,
         'nombre' => 'Mendoza',
         'precio' => '$36000',
         'descripcion' => 'Duración: 4 Días / 3 Noches Salidas: - Jueves Incluye: Aéreos Buenos Aires/Mendoza/Buenos Aires Un equipaje de mano de hasta 8kg Traslados de llegada y de salida Alojamiento según régimen seleccionado.',
-        'url' => 'https://www.mktespider.com.ar/usr/images/amichi/DESTINOS/MDZ/6.jpg'
+        'url' => '../images/mendoza.jpg'
     ),
 );
 
@@ -50,43 +50,43 @@ $exterior = array(
         "id" => 0,
         'nombre' => 'Rio',
         'precio' => '$50000',
-        'descripcion' => 'Programa incluye: Vuelos operados por Gol en clase "O" con equipaje incluido y venta anticipada de 28 días. Traslados IN/OUT en regular. 07 noches de alojamiento según régimen indicado. Opcional: asistencia CORIS con seguro de cancelación USD 36 por persona.',
-        'url' => 'https://www.positivoviajes.tur.ar/usr/images/3625_angra-dos-reis-baja.jpg'
+        'descripcion' => 'incluye: Vuelos operados por Gol con equipaje incluido y venta anticipada. 07 noches de alojamiento según régimen indicado. Opcional: asistencia CORIS con seguro de cancelación USD 36 por persona.',
+        'url' => '../images/rio.jpg'
     ),
     array(
         'id' => 1,
         'nombre' => 'Cancun',
         'precio' => '$45000',
-        'descripcion' => 'Incluye: Ticket aéreo, Traslados, Alojamiento según régimen y Asistencia al viajero.',
-        'url' => 'https://solways.com.ar/usr/images/7497_quintana-roo_destinos-principales_cancun_01.jpg'
+        'descripcion' => 'Incluye: Ticket aéreo, Traslados, Alojamiento según régimen y Asistencia al viajero. alojamiento según régimen indicado. Opcional: asistencia CORIS con seguro de cancelación USD 36 por persona.',
+        'url' => '../images/cancum.jpg'
     ),
     array(
         'id' => 2,
         'nombre' => 'Peru',
         'precio' => '$60000',
-        'descripcion' => 'EL PROGRAMA INCLUYE:   GUÍA: Operado con guía en español e inglés.   ALOJAMIENTO: 3 noches de alojamiento en Lima. 1 noche de alojamiento en Paracas. 2 noches de alojamiento en Cusco. 1 noche de alojamiento en Aguas Calientes. ALIMENTACIÓN: Desayuno diario incluidos.',
-        'url' => 'https://www.positivoviajes.tur.ar/usr/images/3734_AIRE_MAR_Y_TIERRA_OKOK.jpg'
+        'descripcion' => 'INCLUYE: GUÍA: Operado con guía en español e inglés. Pasaje (Latam Airlines).   ALOJAMIENTO: 3 noches de alojamiento en Lima. 1 noche de alojamiento en Paracas. 2 noches de alojamiento en Cusco. 1 noche de alojamiento en Aguas Calientes. ALIMENTACIÓN: Desayuno diario incluidos. Opcional: asistencia CORIS con seguro de cancelación USD 36 por persona.',
+        'url' => '../images/peru.jpg'
     ),
     array(
         'id' => 3,
         'nombre' => 'España',
         'precio' => '$45000',
-        'descripcion' => 'ISITANDO: MADRID - MEAÑO - RIAS BAJAS - SANTIAGO DE COMPOSTELA - PLAYA DE LAS CATEDRALES - RINLO - CABO VIDIO - LA MANJOYA - COVADONGA - CUEVAS DEL SOPLAO - SANTANDER - BILBAO - SAN SEBASTIAN - PAMPLONA - HARO - QUINTANILLA DE ONESIMO.',
-        'url' => 'https://www.sobolviajes.com/usr/images/2843_madrid_BANDERA.jpg'
+        'descripcion' => 'VISITANDO: MADRID, SANTIAGO DE COMPOSTELA -RINLO - CABO VIDIO - LA MANJOYA - COVADONGA - CUEVAS DEL SOPLAO - SANTANDER - BILBAO - SAN SEBASTIAN.',
+        'url' => '../images/spain.jpg'
     ),
     array(
         'id' => 4,
         'nombre' => 'Estados Unidos',
         'precio' => '$20000',
-        'descripcion' => 'Visitando: Boston / Quebec / Montreal / Ottawa / Toronto / Niagara Falls - Incluye: Desayunos Americanos.',
-        'url' => 'https://www.sobolviajes.com/usr/images/2099_Boston_.jpg'
+        'descripcion' => 'Visitando: Boston / Quebec / Montreal / Ottawa / Toronto / Niagara Falls - Incluye: boleto por American Airlines. Desayunos Americanos. ALIMENTACIÓN: Desayuno diario incluidos. Opcional: asistencia CORIS con seguro de cancelación USD 36 por persona.',
+        'url' => '../images/estadosunidos.jpg'
     ),
     array(
         'id' => 5,
         'nombre' => 'Canada',
         'precio' => '$36000',
-        'descripcion' => 'VISITANDO: Vancouver/ Hope / Kelowna / Revelstoke / Golden / Columbia Ice Field / Canmore /  Banff / Calgar.',
-        'url' => 'https://www.sobolviajes.com/usr/images/2221_vancouver-PUERTO.jpg'
+        'descripcion' => 'VISITANDO: Vancouver/ Hope / Kelowna / Revelstoke / Golden / Columbia Ice Field / Canmore /  Banff / Calgar. Incluye: vuelo por American Airlines (Clase turista).',
+        'url' => '../images/canada.jpg'
     ),
 );
 

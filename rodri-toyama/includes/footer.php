c0b4368ba2c8dfd8d3ac2d0a4757beb7aefe8e14
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-26 03:56:49 -0300

    parte de linkintereses movido a footer

--- rodri-toyama/includes/footer.php
+++ rodri-toyama/includes/footer.php
@@ -1,3 +1,21 @@
+<div class="container-fluid p-0 text-center">
+    <div class="container-fluid" id="tab-content-1">
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div class="tab-0a"><a class="text-light" href="somos.php">Quienes Somos</a></div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div class="tab-1a"><a class="text-light" href="formadepago.php">Forma De Pago</a></div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div class="tab-2a"><a class="text-light" href="protecciondatos.php">Protección Datos Personales</a></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
 <footer>
     <div class="container-fluid text-white">
 
@@ -66,6 +84,7 @@
 
     </div>
 </footer>
+
 
 <!-- Scripts -->
 <script src="./js/jquery.min.js"></script>

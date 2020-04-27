f47d596436da5f6d7694748a3e818965514e6d1c
Roberto Rossa <rcrossa@hotmail.com>
2020-04-27 11:38:51 -0300

    update jquery index rodri-toyama
    Se coloca script de jquery por tipyerror en la consola de debug. Queda solucionado ese error queda pendiente resolver lo que esta mal parseado.

--- index.php
+++ index.php
@@ -40,7 +40,8 @@
     ?>
 
     <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="js/popper/popper.min.js"></script>
     <script src="js/bootstrap/bootstrap.min.js"></script>
 

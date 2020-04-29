109459fbd088dc58e988c43a3bd72f0e60961535
Alberto Bossio <alberto.federico.bossio@gmail.com>
2020-04-29 17:34:15 -0300

    alberto-bossio arreglado

--- alberto-bossio/archivosphp/funcionesfiltro.php
+++ alberto-bossio/archivosphp/funcionesfiltro.php
@@ -1,4 +1,4 @@
-<!--
+
 <!DOCTYPE html>
 <html>
 
@@ -36,19 +36,6 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-
-
 </script>
 
 </html>
--->
--- alberto-bossio/filtro.php
+++ alberto-bossio/filtro.php
@@ -1,38 +1,25 @@
 <div class="container pt-4 px-5">
     <div class="row justify-content-center">
 
+        <div class="col-12 text-center pt-5 pb-4">
+            <h1>Catálogo</h1>
+        </div>
+
+            <svg width="20%" height="2">
+                <rect width="100%" height="100" style="fill:rgb(255,165,0);stroke-width:0;stroke:rgb(0,0,0)" />
+            </svg>
+
         <div class="col-12">
-            <div class="filter-wrap py-4">
+        
                 <h3>Filtro</h3>
-                <div class="filter-border p-4 border border-secondary">
-                    <div class="filter-inner">
-                        <div class="filtrolugar">
-                            <div class="row justify-content-center align-items-center">
 
-                                <div id="myBtnContainer">
-                                    <option class="btn" onclick="filterSelection('Todo')"> Todo </option>
-                                    <option class="btn" onclick="filterSelection('Interior')"> Interior </option>
-                                    <option class="btn" onclick="filterSelection('Exterior')"> Exterior </option>
-                                </div>
-
-                                <div class="custom-select custom-select-lg">
-                                <div class="filterDiv Interior">chubut</div>
-                                <div class="filterDiv Interior">catamarca</div>
-                                <div class="filterDiv Interior">jujuy</div>
-                                <div class="filterDiv Exterior">españa</div>
-                                <div class="filterDiv Exterior">estados unidos</div>
-                                <div class="filterDiv Exterior">francia</div>
-                                </div>
-
-                                <div class="col-sm-12 col-lg-2 py-2">
-                                    <button class="btn btn-danger btn-md btn-block px-4">Buscar</button>
-                                </div>
-
-                            </div>
+                    <div class="bg-warning">
+                        <div id="myBtnContainer">
+                            <button class="btn" onclick="filterSelection('Todo')"> Todo</button>
+                            <button class="btn" onclick="filterSelection('Interior')"> Interior</button>
+                            <button class="btn" onclick="filterSelection('Exterior')"> Exterior</button>
                         </div>
                     </div>
-                </div>
-            </div>
         </div>
 
     </div>

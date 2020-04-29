71f69f187ee581aab5e89dc307768610690f55ec
Roberto Rossa <rcrossa@hotmail.com>
2020-04-29 16:43:48 -0300

    correccion
    correccion

---
+++ alberto-bossio/Catalogo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once "head.php"; ?>
+    <title>Delfos Tour</title>
+</head>
+<body>
+<?php
+    require_once "archivosphp/funcionesproductoportada.php";
+    // archivos html
+    require_once "encabezado.php";
+    require_once "filtros.php";
+    require_once "productoportada.php";
+    require_once "linkinteresesyherramientas.php";
+    require_once "footer0.php";
+    require_once "footer.php";
+    ?>
+
+
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="js/popper/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ alberto-bossio/productos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php require_once "head.php"; ?>
+    <title>Catálogo</title>
+</head>
+
+<body>
+    <?php
+    require_once "archivosphp/funcionesproductoportada.php";
+
+    // archivos html
+    $page = 'catalogo';
+    require_once "encabezado.php";
+    require_once "filtro.php";
+    require_once "productoportada.php";
+    require_once "linkinteres.php";
+    require_once "footer.php";
+    ?>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="js/popper/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+
+</body>
+
+</html>

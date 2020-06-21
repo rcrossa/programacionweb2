86b7b55ce787b32da8862b8ad275a452621684e9
Roberto Rossa <rcrossa@hotmail.com>
2020-06-20 23:34:41 -0300

    borrado de carpetas adicionales
    Se realiza el eliminado de carpetas que no pertenecen a la version final

--- rodri-toyama/includes/pruebaciudades.php
+++
@@ -1,8 +0,0 @@
-<select name="ciudad" class="custom-select custom-select-lg" id="third-choice">
-    <option>Todo</option>
-    <?php foreach ($productos as $producto) {
-        foreach ($producto['ciudades'] as $key => $value) { ?>
-            <option><?php echo $value; ?></option>
-        <?php } ?>
-    <?php } ?>
-</select>
